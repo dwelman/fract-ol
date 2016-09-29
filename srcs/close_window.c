87f2562c7de09df27e3cb6cd97bcac4feccace21
David WELMAN <daviwel@e5r7p1.jnb.42.fr>
2016-09-29 08:30:45 +0200

    a

--- srcs/close_window.c
+++ srcs/close_window.c
@@ -6,7 +6,7 @@
 /*   By: daviwel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/01 08:32:34 by daviwel           #+#    #+#             */
-/*   Updated: 2016/09/27 10:51:44 by daviwel          ###   ########.fr       */
+/*   Updated: 2016/09/29 07:59:25 by daviwel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 int	close_window(t_env *env)
 {
 	(void)env;
-	//Add memory cleanup!
-	//mlx_destroy_window(env->mlx, env->img.img);
 	exit(0);
 	return (0);
 }
